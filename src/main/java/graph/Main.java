dea0b4ce3ff98795deaa5c2bb966c9e9c59e6c9b
Pavel <pavelb23@gmail.com>
2016-09-05 09:22:45 +0300

    Add some model.

--- src/main/java/graph/Main.java
+++ src/main/java/graph/Main.java
@@ -2,22 +2,24 @@ package graph;
 
 import org.neo4j.driver.v1.*;
 
+import java.util.stream.Collectors;
+
 /**
  * Created by pbezglasnyi.
  */
 public class Main {
 
     public static void main(String[] args) {
-        Driver driver = GraphDatabase.driver( "bolt://localhost", AuthTokens.basic( "neo4j", "postgres" ) );
+        Driver driver = GraphDatabase.driver("bolt://localhost", AuthTokens.basic("neo4j", "postgres"));
         Session session = driver.session();
 
-        session.run( "CREATE (a:Person {name:'Arthur', title:'King'})" );
+//        session.run( "CREATE (a:Person {name:'Arthur', title:'King'})" );
 
-        StatementResult result = session.run( "MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title" );
-        while ( result.hasNext() )
-        {
+        StatementResult result = session.run("MATCH (p:Point) RETURN ID(p) as name");
+        while (result.hasNext()) {
             Record record = result.next();
-            System.out.println( record.get( "title" ).asString() + " " + record.get("name").asString() );
+//            System.out.println(record.values().stream().map(v->v.containsKey("name")).collect(Collectors.toList()));
+            System.out.println(record.get("name").asInt());
         }
 
         session.close();
